e51c15f17447a50a24863111a5f34ec6ab26bde3
Andres Buritica <andres@thelinuxkid.com>
2013-05-08 18:12:39 -0700

    Add jsonformat.totime and jsonformat.fromtime
    Converts times relative to the Ripple epoch

--- ripple/jsonformat.py
+++ ripple/jsonformat.py
@@ -1,3 +1,10 @@
+import math
+
+from dateutil import tz
+from datetime import datetime, timedelta
+
+ripple_epoch = datetime(2000, 1, 1, tzinfo=tz.tzutc())
+
 def drop(drops):
     """
     Converts a string to drops. A drop the smallest unit in the Ripple
@@ -13,3 +20,40 @@ def drop(drops):
     # in -sys.maxint - 1 <= x <= sys.maxint
     drops = int(drops)
     return drops
+
+def totime(dt):
+    """
+    Converts a datetime object to the number of seconds since
+    the Ripple epoch. The Ripple epoch is 946684800 seconds after the
+    Unix epoch or 2000-01-01T00:00:00+00:00
+    """
+    if dt.tzinfo is None:
+        # We cannot convert to UTC without knowing a timezone
+        # A lack of timezone implies local time.
+        # http://en.wikipedia.org/wiki/ISO_8601#Time_zone_designators
+        raise ValueError(
+            'Value must contain timezone information: '
+            '{dt}'.format(
+                dt=dt.isoformat(),
+            )
+        )
+    if dt < ripple_epoch:
+        raise ValueError(
+            'Value cannot be earlier than {epoch}: {dt}'.format(
+                epoch=ripple_epoch.isoformat(),
+                dt=dt.isoformat(),
+            )
+        )
+    dt = dt - ripple_epoch
+    dt = dt.total_seconds()
+    dt = math.ceil(dt)
+    return long(dt)
+
+def fromtime(seconds):
+    """
+    Converts the number of seconds since the Ripple epoch to a
+    datetime object. The Ripple epoch is 946684800 seconds after the
+    Unix epoch or 2000-01-01T00:00:00+00:00
+    """
+    dt = ripple_epoch + timedelta(seconds=seconds)
+    return dt
--- ripple/test/test_jsonformat.py
+++ ripple/test/test_jsonformat.py
@@ -1,5 +1,8 @@
 import sys
 
+from datetime import datetime
+
+from dateutil import tz
 from nose.tools import assert_raises
 
 from ripple import jsonformat
@@ -54,3 +57,35 @@ def test_drop_sys_min():
         min_value=min_value,
     )
     assert exc.message == expect
+
+def test_totime_simple():
+    dt = datetime(2013, 5, 9, 0, 55, 24, tzinfo=tz.tzutc())
+    res = jsonformat.totime(dt)
+    expect = 421376124
+    assert res == expect
+
+def test_totime_earlier():
+    dt = datetime(1999, 5, 9, 0, 55, 24, tzinfo=tz.tzutc())
+    with assert_raises(ValueError) as exc:
+        jsonformat.totime(dt)
+    exc = exc.exception
+    expect = (
+        'Value cannot be earlier than 2000-01-01T00:00:00+00:00: '
+        '1999-05-09T00:55:24+00:00'
+    )
+    assert exc.message == expect
+
+def test_totime_notz():
+    dt = datetime(2013, 5, 9, 0, 55, 24)
+    with assert_raises(ValueError) as exc:
+        jsonformat.totime(dt)
+    exc = exc.exception
+    expect = (
+        'Value must contain timezone information: 2013-05-09T00:55:24'
+    )
+    assert exc.message == expect
+
+def test_fromtime_simple():
+    res = jsonformat.fromtime(421376124)
+    expect = datetime(2013, 5, 9, 0, 55, 24, tzinfo=tz.tzutc())
+    assert res == expect
