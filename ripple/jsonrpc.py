ca6000a4be5a38c00570f62fdc64532e5140cd86
Andres Buritica <andres@thelinuxkid.com>
2013-05-09 18:39:10 -0700

    Add jsonrpc.check_options decorator

--- ripple/jsonrpc.py
+++ ripple/jsonrpc.py
@@ -1,5 +1,9 @@
 from functools import wraps
 
+unique_options = dict([
+    ('ledger', ['ledger_hash', 'ledger_index']),
+])
+
 
 class RippleRPCError(Exception):
     """"
@@ -47,4 +51,53 @@ def check_result(fn):
                 message=result['error_message']
             )
         return fn(result)
+    return wrapper
+
+
+def check_options(*options):
+    """
+    Raise if the request contains more than one option in an option
+    set. For example, a request should only contain one of ledger_hash
+    or ledger_index.
+    """
+    def wrapper(fn):
+        @wraps(fn)
+        def wrapped(*args, **kwargs):
+            if not options:
+                raise ValueError(
+                    'At least one option set is needed: '
+                    '{need}'.format(
+                        need=', '.join(unique_options.keys())
+                    )
+                )
+            check = [
+                v for (k, v) in unique_options.items()
+                if k in options
+            ]
+            if len(options) != len(check):
+                diff = set(options) - set(unique_options.keys())
+                raise ValueError(
+                    'Invalid option set: {options}'.format(
+                        options=', '.join(diff)
+                    )
+                )
+            for unique in check:
+                found = [
+                    k for k in kwargs.keys() if k in unique
+                ]
+                if not found:
+                    raise ValueError(
+                        'At least one option is needed: {need}'.format(
+                            need=', '.join(unique)
+                        )
+                    )
+                if len(found) > 1:
+                    raise ValueError(
+                        'Only one option can be specified: '
+                        '{need}'.format(
+                            need=', '.join(unique),
+                        )
+                    )
+            return fn(*args, **kwargs)
+        return wrapped
     return wrapper
--- ripple/test/test_jsonrpc.py
+++ ripple/test/test_jsonrpc.py
@@ -1,4 +1,8 @@
-from nose.tools import assert_raises
+from nose.tools import (
+    assert_raises,
+    assert_dict_equal,
+    assert_tuple_equal,
+)
 from nose.tools import eq_ as equal
 
 from ripple import jsonrpc
@@ -74,4 +78,91 @@ def test_check_result_error():
         _test(response)
     exc = exc.exception
     expect = 'foo_name, foo_code: foo_message'
+    equal(str(exc), expect)
+
+
+def test_check_options_simple():
+    @jsonrpc.check_options('ledger')
+    def _test(*args, **kwargs):
+        expect = ('foo', 'bar')
+        assert_tuple_equal(args, expect)
+        expect = dict([
+            ('baz', 'qux'),
+            ('ledger_hash', 'foo_ledger'),
+        ])
+        assert_dict_equal(kwargs, expect)
+    _test('foo', 'bar', baz='qux', ledger_hash='foo_ledger')
+
+
+def test_check_options_missing():
+    @jsonrpc.check_options('ledger')
+    def _test(*args, **kwargs):
+        raise AssertionError('Function should not have been called')
+    with assert_raises(ValueError) as exc:
+        _test('foo', 'bar', baz='qux')
+    exc = exc.exception
+    expect = (
+        'At least one option is needed: ledger_hash, ledger_index'
+    )
+    equal(str(exc), expect)
+
+
+def test_check_options_many():
+    @jsonrpc.check_options('ledger')
+    def _test(*args, **kwargs):
+        raise AssertionError('Function should not have been called')
+    with assert_raises(ValueError) as exc:
+        _test(
+            'foo',
+            'bar',
+            baz='qux',
+            ledger_hash='foo',
+            ledger_index='bar',
+        )
+    exc = exc.exception
+    expect = (
+        'Only one option can be specified: ledger_hash, ledger_index'
+    )
+    equal(str(exc), expect)
+
+
+def test_check_options_invalid():
+    @jsonrpc.check_options('foo', 'ledger')
+    def _test(*args, **kwargs):
+        raise AssertionError('Function should not have been called')
+    with assert_raises(ValueError) as exc:
+        _test()
+    exc = exc.exception
+    expect = 'Invalid option set: foo'
+    equal(str(exc), expect)
+
+
+def test_check_options_empty():
+    @jsonrpc.check_options()
+    def _test(*args, **kwargs):
+        raise AssertionError('Function should not have been called')
+    with assert_raises(ValueError) as exc:
+        _test()
+    exc = exc.exception
+    expect = 'At least one option set is needed: ledger'
+    equal(str(exc), expect)
+
+
+def test_check_options_multiple():
+    jsonrpc.unique_options = dict([
+        ('foo', ['foo_hash', 'foo_index']),
+        ('bar', ['bar_hash', 'bar_index']),
+    ])
+
+    @jsonrpc.check_options('foo', 'bar')
+    def _test(*args, **kwargs):
+        raise AssertionError('Function should not have been called')
+    with assert_raises(ValueError) as exc:
+        _test(
+            foo_hash='foo hash',
+            bar_hash='bar hash',
+            bar_index='bar index',
+        )
+    exc = exc.exception
+    expect = 'Only one option can be specified: bar_hash, bar_index'
     equal(str(exc), expect)
